8fb9b6162f49ed887e6ab792f1a52228244b5588
Oscar Hernandez <oscarhernandez@Mac-mini-de-Administrador.local>
2022-09-01 16:21:38 +0200

    Another Way

--- tests/PersonTest.php
+++ tests/PersonTest.php
@@ -6,25 +6,53 @@ use PHPUnit\Framework\TestCase;
 
 class PersonTest extends TestCase 
 {
+    // /**
+    // * @dataProvider namesProvider
+    // */
+    // public function testCreate($name, $new_name) 
+    // {
+    //     $this->person = new Person($name);
+    //     $this->assertSame($this->person->GetName(), $name);
+    //     $this->assertSame($this->person->greeting(), "Hello, I'm {$name}!");
+    //     $this->assertSame($this->person->setName($new_name), null);
+    //     $this->assertSame($this->person->GetName(), $new_name);
+    // }
+
+    public function testCreate() 
+    {
+        $name = "John";
+        $person = new Person($name);
+        $this->assertSame($person->GetName(), $name);
+        return $person;
+    }
+
     /**
     * @dataProvider namesProvider
+    * @depends testCreate
     */
-    public function testCreate($name, $new_name) 
+    public function testGreeting($name, $person) 
     {
-        $this->person = new Person($name);
-        $this->assertSame($this->person->GetName(), $name);
-        $this->assertSame($this->person->greeting(), "Hello, I'm {$name}!");
-        $this->assertSame($this->person->setName($new_name), null);
-        $this->assertSame($this->person->GetName(), $new_name);
+        $this->assertSame($person->greeting(), "Hello, I'm {$person->GetName()}!");
     }
+
+    /**
+    * @dataProvider namesProvider
+    * @depends testCreate
+    */
+    public function testSetName($name, $person )
+    {
+        $this->assertSame($person->setName($name), null);
+        $this->assertSame($person->GetName(), $name);
+    }
+
 
     public function namesProvider()
     {
         return [
-            "Case 1" => ["John", "Bill"],
-            "Case 2" => ["Mark", "Arthur"],
-            "Case 3" => ["Alan", "Thomas"],
-            "Case 4" => ["Dijkstra", "Oscar"]
+            "Case 1" => ["John"],
+            "Case 2" => ["Mark"],
+            "Case 3" => ["Alan"],
+            "Case 4" => ["Dijkstra"]
         ];
     }
 }
